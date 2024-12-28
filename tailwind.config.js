3d92e8bee81ed0279a15a68adcd636091cd664f9
Scott Sigman <ssigman@deuey.wdu>
2024-12-28 11:05:01 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html", "./build/js/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        'ccsc-dark-blue': '#01224d',
+        'ccsc-medium-blue': '#005a98',
+        'ccsc-light-blue': '#43b5d0',
+        'ccsc-gold': '#feb02a',
+        'ccsc-gray': {
+          light: '#D3D3D3',
+          DEFAULT: '#767374'
+        },
+      },
+      screens: {
+        'widescreen': { 'raw': '(min-aspect-ratio: 3/2)' },
+        'tallscreen': { 'raw': '(max-aspect-ratio: 13/20)' },
+      },
+      keyframes: {
+        'open-menu': {
+          '0%': { transform: 'scaleY(0)' },
+          '80%': { transform: 'scaleY(1.2)' },
+          '100%': { transform: 'scaleY(1)' },
+        },
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
+      }
+    },
+  },
+  plugins: [],
+}
+
